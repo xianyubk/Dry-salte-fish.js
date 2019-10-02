95d89ebdca0f5664c14d1340e2315ede4bda8de2
ZZY2357 <47471318+ZZY2357@users.noreply.github.com>
2019-10-02 22:48:03 +0800

    添加了`dried()`函数。并添加了基础的压缩能力

--- src/fish.js
+++ src/fish.js
@@ -1,1 +1,37 @@
+/**
+ * fish.js源文件
+ */
 
+
+/**
+ * 用来去掉换行
+ */
+function removeLines(_code) {
+    let code = _code
+    // 把所有换行符转换成空格
+    code = code.replace(/\n/g, ' ');
+
+    return code;
+}
+
+/**
+ * 用来去掉缩进
+ */
+function removeTabs(_code) {
+    let code = _code
+    // 把所有缩进转换成空格
+    code = code.replace(/\t/g, ' ');
+
+    return code;
+}
+
+/** 
+ * 压缩的主函数 
+ */
+function dried(_code) {
+    let code = _code;
+    code = removeLines(code);
+    code = removeTabs(code);
+
+    return code;
+}
