1dbbb16ecf10c2a726035ff55780075103ac99ec
ZZY2357 <47471318+ZZY2357@users.noreply.github.com>
2019-10-07 00:51:56 -0500

    修补了单行注释删除不了而导致意外后果的bug

--- src/fish.js
+++ src/fish.js
@@ -1,11 +1,22 @@
-/**
- * fish.js源文件
- * 如果你想帮助修改，请学习 "正则表达式"
+/*!
+ * Dry-salte-fish
+ * version: 1.0.0
  */
 
+/**
+ * 把所有注释删除
+ * @param {string} _code - 要压缩的代码
+ */
+function removeAnnotation(_code) {
+    let code = _code;
+    // 把所有注释删除
+    code = code.replace(/\/\/.*\n/g, '');
+    return code;
+}
 
 /**
  * 将所有(换行符(\n)、制表符(\t)、一个以上空格( +))(\s)换成单个空格
+ * @param {string} _code - 要压缩的代码
  */
 function removeWhite(_code) {
     let code = _code;
@@ -18,9 +29,11 @@ function removeWhite(_code) {
 
 /** 
  * 压缩的主函数 
+ * @param {string} _code - 要压缩的代码
  */
 function dry(_code) {
     let code = _code;
+    code = removeAnnotation(code);
     code = removeWhite(code);
 
     return code;
